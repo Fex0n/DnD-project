ef16448fb48a9bfe7678aa86c616f3162c3bc997
Fex0n <fam638310@gmail.com>
2025-06-26 20:17:51 +0300

    ааашшшштффффдддд

--- script.js
+++ script.js
@@ -80,3 +80,14 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+// Чат
+    (function(d, w, c) {
+        w.ChatraID = 'xFokWo8qPeqMFCd28';
+        var s = d.createElement('script');
+        w[c] = w[c] || function() {
+            (w[c].q = w[c].q || []).push(arguments);
+        };
+        s.async = true;
+        s.src = 'https://call.chatra.io/chatra.js';
+        if (d.head) d.head.appendChild(s);
+    })(document, window, 'Chatra');
